c751b98689b7525d7e297751deb544e826f2d333
zwoong <zwoong6867@gmail.com>
2025-03-26 19:26:03 +0900

    ✨ CommentJava

---
+++ src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력합니다.
+        // system.out.println("hello java2"); 한 줄 주석 - 라인 전체 적용
+
+        /* 여러 줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
