07834d20bcc69b10a0e6e708fa9e090812748912
zwoong <zwoong6867@gmail.com>
2025-03-26 22:37:24 +0900

    ✨ Val1

---
+++ src/variable/Val1.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Val1 {
+    public static void main(String[] args) {
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+    }
+}
