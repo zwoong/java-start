b9bcaead39a30a4396fff45677d5d667e7180bbf
zwoong <zwoong6867@gmail.com>
2025-03-27 09:12:33 +0900

    ✨ Operator1

---
+++ src/operator/Operator1.java
@@ -0,0 +1,29 @@
+package operator;
+
+public class Operator1 {
+     public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("sum = " + sum); // a + b = 7
+
+        // 뺼셈
+        int diff = a - b;
+        System.out.println("diff = " + diff);
+
+        // 곱셈
+        int multi = a * b;
+        System.out.println("multi = " + multi);
+
+        // 나눈셈
+        int div = a / b;
+        System.out.println("div = " + div);
+
+        // 나머지
+        int mod = a % b;
+        System.out.println("mod = " + mod);
+    }
+}
