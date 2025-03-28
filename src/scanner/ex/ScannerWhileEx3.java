a0c6de58c1a495055f73973f0483399127e72ea2
zwoong <zwoong6867@gmail.com>
2025-03-28 19:17:20 +0900

    ✨ ScannerWhileEx3

---
+++ src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,26 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+        int count = 0;
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요:");
+
+        while (true) {
+            int number = input.nextInt();
+            if (number == -1) {
+                double average = (double) sum / count;
+                System.out.println("입력한 숫자들의 평균: " + average);
+                break;
+            }
+            sum += number;
+            count++;
+        }
+
+    }
+}
