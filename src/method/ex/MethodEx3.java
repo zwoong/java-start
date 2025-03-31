ebd4fb021e14d594d18520340ce276ed60a8f7f5
zwoong <zwoong6867@gmail.com>
2025-03-31 19:44:57 +0900

    ✨ MethodEx3Ref

---
+++ src/method/ex/MethodEx3.java
@@ -0,0 +1,21 @@
+package method.ex;
+
+public class MethodEx3 {
+    public static void main(String[] args) {
+        int balance = 10000;
+
+        int deposiAmount = 1000;
+        balance += deposiAmount;
+        System.out.println(deposiAmount + "원을 입금하였습니다. 현재 잔액: " + balance + "원");
+
+        int withdrawAmount = 2000;
+        if (balance >= withdrawAmount) {
+            balance -= withdrawAmount;
+            System.out.println(withdrawAmount + "원을 출금하였습니다. 현재 잔액: " + balance + "원");
+        } else {
+            System.out.println(withdrawAmount + "원을 출금하려 했으나 잔액이 부족합니다.");
+        }
+
+        System.out.println("최종 잔액: " + balance + "원");
+    }
+}
